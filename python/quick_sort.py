b858e411b83fb031626dcac5dd9918e5a37950e3
Nghia <ntran4298@gmail.com>
2018-06-05 20:52:00 -0400

    doctest

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -6,6 +6,8 @@ def quick_sort(array):
 	quick_sort 
 	>>> quick_sort([1,2,3,2352,2342])
 	[1, 2, 3, 2342, 2352]
+	>>> quick_sort([12,2323,3,2352,2342])
+	[3, 12, 2323, 2342, 2352]
 	"""
 	array_length = len(array)
 	if array_length <= 1:
